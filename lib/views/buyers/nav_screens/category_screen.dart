70a2b17f1b2dfe0cf1358ea6544f174edd7fbf3e
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-05-08 20:46:25 -0300

    display categorie finish

--- lib/views/buyers/nav_screens/category_screen.dart
+++ lib/views/buyers/nav_screens/category_screen.dart
@@ -1,12 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Category Screen'),
+    final Stream<QuerySnapshot> _categoriesStream =
+        FirebaseFirestore.instance.collection('categories').snapshots();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: Text(
+          'Categories',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3,
+          ),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _categoriesStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Colors.yellow.shade900,
+              ),
+            );
+          }
+          return Container(
+            height: 200,
+            child: ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final categoryData = snapshot.data!.docs[index];
+                return ListTile(
+                  leading: Image.network(categoryData['image']),
+                  title: Text(categoryData['categoryName']),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
