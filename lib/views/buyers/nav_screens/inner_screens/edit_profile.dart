7636c552c7962610aa5c6a2e7fc1cabf2df44209
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-05-19 22:36:39 -0300

    update address

--- lib/views/buyers/nav_screens/inner_screens/edit_profile.dart
+++ lib/views/buyers/nav_screens/inner_screens/edit_profile.dart
@@ -1,10 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-class EditProfileScreen extends StatelessWidget {
+class EditProfileScreen extends StatefulWidget {
   final dynamic userData;
 
-  const EditProfileScreen({super.key, required this.userData});
+  EditProfileScreen({super.key, required this.userData});
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final TextEditingController _fullNameController = TextEditingController();
+
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+
+  String? address;
+
+  @override
+  void initState() {
+    setState(() {
+      _fullNameController.text = widget.userData['fullName'];
+      _emailController.text = widget.userData['email'];
+      _phoneNumberController.text = widget.userData['phoneNumber'];
+      _addressController.text = widget.userData['address'];
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,68 +51,112 @@ class EditProfileScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundColor: Colors.yellow.shade900,
-                  ),
-                  Positioned(
-                    right: 0,
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        CupertinoIcons.photo,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.yellow.shade900,
+                    ),
+                    Positioned(
+                      right: 0,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          CupertinoIcons.photo,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  initialValue: userData['fullName'],
-                  decoration: InputDecoration(
-                    labelText: 'Enter Full Name',
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _fullNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter Full Name',
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  initialValue: userData['email'],
-                  decoration: InputDecoration(
-                    labelText: 'Enter Email',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter Email',
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  initialValue: userData['phoneNumber'],
-                  decoration: InputDecoration(
-                    labelText: 'Enter PhoneNumber',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _phoneNumberController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter PhoneNumber',
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  initialValue: userData['address'],
-                  decoration: InputDecoration(
-                    labelText: 'Enter Address',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        address = value;
+                      });
+                    },
+                    controller: _addressController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter Address',
+                    ),
                   ),
                 ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: InkWell(
+          onTap: () async {
+            EasyLoading.show(status: 'Waiting...');
+            await _firestore
+                .collection('buyers')
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .update({
+              'fullName': _fullNameController.text,
+              'email': _emailController.text,
+              'phoneNumber': _phoneNumberController.text,
+              'address': address,
+            }).whenComplete(() {
+              EasyLoading.dismiss();
+            });
+          },
+          child: Container(
+            height: 40,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.yellow.shade900,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                'UPDATE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  letterSpacing: 6,
+                ),
               ),
-            ],
+            ),
           ),
         ),
       ),
